6ecac5e9b58ee1a6e1bf7ba28b1df25d3af8ce8f
Peter Lu <peterlu195@yahoo.com>
2016-09-20 21:12:39 -0700

    PROGRESSgit add .!

--- js/apps.js
+++ js/apps.js
@@ -1,3 +1,13 @@
 /**
  * Created by Peter on 9/20/2016.
  */
+
+$(function() {
+   'use strict';
+    $('h1').hide().fadeIn();
+    var titlerect = $('#titlerect');
+    titlerect.css('width', '200 px');
+    console.log(innerWidth - titlerect.position().left);
+    console.log(innerWidth);
+
+});
