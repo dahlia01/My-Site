b453a89e5919fcaf7fcdc795836ce4b3a5a7abad
Peter Lu <peterlu195@yahoo.com>
2016-09-21 19:07:32 -0700

    tile fades in

--- js/apps.js
+++ js/apps.js
@@ -1,13 +1,59 @@
 /**
  * Created by Peter on 9/20/2016.
  */
+$(document).ready(function() {
+    $(function () {
+        //$('.col-sm-4').fadeIn(1000);
+        //$('.col-sm-4').fadeTo(20000, 100);
 
-$(function() {
-   'use strict';
-    $('h1').hide().fadeIn();
-    var titlerect = $('#titlerect');
-    titlerect.css('width', '200 px');
-    console.log(innerWidth - titlerect.position().left);
-    console.log(innerWidth);
+        function fadeInIterator(els) {
+            //$('#UX div.col-sm-4').each(function(i, el) {
+            $(els).each(function(i, el) {
+                setTimeout(function() {
+                    $(el).fadeTo(7000,100);
+                }, 300 + (i * 300));
+            })
+        }
 
+        'use strict';
+
+
+        //returns if a given elem has scrolled into view
+        function isScrolledIntoView(elem) {
+            var docViewTop = $(window).scrollTop();
+            var docViewBottom = docViewTop + $(window).height();
+
+            var elemTop = $(elem).offset().top;
+            var elemBottom = elemTop + $(elem).height();
+
+            return ((elemTop + 100 <=docViewBottom));
+        }
+
+
+
+        $(window).scroll(function() {
+            if(isScrolledIntoView('#UX')) {
+                fadeInIterator('#UX div.col-sm-4')
+            }
+            if(isScrolledIntoView('#DATA')) {
+                fadeInIterator('#DATA div.col-sm-4')
+            }
+            if(isScrolledIntoView('#WEB')) {
+                fadeInIterator('#WEB div.col-sm-4')
+            }
+
+            $('.fadeInBlock').each( function(i){
+
+                var bottom_of_object = $(this).position().top + $(this).outerHeight();
+                var bottom_of_window = $(window).scrollTop() + $(window).height();
+
+                /* Adjust the "200" to either have a delay or that the content starts fading a bit before you reach it  */
+                bottom_of_window = bottom_of_window + 200;
+
+                if(isScrolledIntoView(this) ){
+                    $(this).animate({'opacity':'1'},1000);
+                }
+            });
+        })
+    });
 });
